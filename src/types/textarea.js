3aa49a64536fc9f9b9622526cdcd6beff5f88b53
gswilcox01 <gary@dugan-wilcox.com>
2017-11-07 20:20:56 -0600

    Array or String allowed for textarea type now

--- src/types/textarea.js
+++ src/types/textarea.js
@@ -1,15 +1,23 @@
 module.exports = {
   html: function (elemId, name, value, meta) {
-    var html = '<textarea id="' + elemId + '" rows=6 style="white-space: nowrap; overflow-x: auto; width:100%">';
+    var html = '<textarea id="' + elemId;
     if (value instanceof Array) {
+      html += '" rows=6 style="white-space: nowrap; overflow-x: auto; width:100%">';
       html += value.join("\n");
+    } else {
+      html += '" rows=6 style="overflow-x: auto; width:100%">';
+      html += value;
     }
     html += '</textarea>';
     return html;
   },
   valueFunction: function (elemId, name, value, meta) {
     return function () {
-      return $('#' + elemId).val().split('\n');
+      if (value instanceof Array) {
+        return $('#' + elemId).val().split('\n');
+      } else {
+        return $('#' + elemId).val()
+      }
     }
   }
 }
